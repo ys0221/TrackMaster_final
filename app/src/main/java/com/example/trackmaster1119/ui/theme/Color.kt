7bf16afe22dc41fe6c5e5c390398fde59e052139
jyjbn1789 <jyjbn1789@gmail.com>
2024-12-07 01:04:35 +0900

    편의시설 이미지 추가

--- app/src/main/java/com/example/trackmaster1119/ui/theme/Color.kt
+++ app/src/main/java/com/example/trackmaster1119/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.trackmaster.ui.theme
+package com.example.trackmaster1119.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
--- app/src/main/java/com/example/trackmaster1119/ui/theme/Type.kt
+++ app/src/main/java/com/example/trackmaster1119/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.trackmaster.ui.theme
+package com.example.trackmaster1119.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
